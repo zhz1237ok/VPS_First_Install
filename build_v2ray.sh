8711169ab60e728898c50eba6342a8251a198153
SekiBetu <realsekibetu@gmail.com>
2021-03-11 20:11:22 +0800

    update

--- build_v2ray.sh
+++ build_v2ray.sh
@@ -26,28 +26,28 @@ echo "编译 v2ray-linux-64 中"
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -ldflags "-s -w" -o v2ray-linux-64/v2ray ./main
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -ldflags "-s -w" -tags confonly -o v2ray-linux-64/v2ctl ./infra/control/main
 echo "使用 7z 打包中"
-7zz a ${cur_dir}/releases/v2ray-linux-64.7z v2ray v2ctl -mx=9 -ms -mf -mhc -mhcf -mmt -r
+7zz a ${cur_dir}/releases/v2ray-linux-64.7z v2ray v2ctl -mx=9 -r
 
 echo "编译 v2ray-linux-arm64-v8a 中"
 cd ${cur_dir}/v2ray-core-4.27.5/
 env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -ldflags "-s -w" -o v2ray-linux-arm64-v8a/v2ray ./main
 env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -ldflags "-s -w" -tags confonly -o v2ray-linux-arm64-v8a/v2ctl ./infra/control/main
 echo "使用 7z 打包中"
-7zz a ${cur_dir}/releases/v2ray-linux-arm64-v8a.7z v2ray v2ctl -mx=9 -ms -mf -mhc -mhcf -mmt -r
+7zz a ${cur_dir}/releases/v2ray-linux-arm64-v8a.7z v2ray v2ctl -mx=9 -r
 
 echo "编译 v2ray-macos-64 中"
 cd ${cur_dir}/v2ray-core-4.27.5/
 env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -ldflags "-s -w" -o v2ray-macos-64/v2ray ./main
 env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -ldflags "-s -w" -tags confonly -o v2ray-macos-64/v2ctl ./infra/control/main
 echo "使用 7z 打包中"
-7zz a ${cur_dir}/releases/v2ray-macos-64.7z v2ray v2ctl -mx=9 -ms -mf -mhc -mhcf -mmt -r
+7zz a ${cur_dir}/releases/v2ray-macos-64.7z v2ray v2ctl -mx=9 -r
 
 echo "编译 v2ray-macos-arm64 中"
 cd ${cur_dir}/v2ray-core-4.27.5/
 env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -v -ldflags "-s -w" -o v2ray-macos-arm64-v8a/v2ray ./main
 env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -v -ldflags "-s -w" -tags confonly -o v2ray-macos-arm64-v8a/v2ctl ./infra/control/main
 echo "使用 7z 打包中"
-7zz a ${cur_dir}/releases/v2ray-macos-arm64-v8a.7z v2ray v2ctl -mx=9 -ms -mf -mhc -mhcf -mmt -r
+7zz a ${cur_dir}/releases/v2ray-macos-arm64-v8a.7z v2ray v2ctl -mx=9 -r
 
 echo "编译 v2ray-windows-64 中"
 cd ${cur_dir}/v2ray-core-4.27.5/
@@ -55,7 +55,7 @@ env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o v2ray-windows-64/v2ray.e
 env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o v2ray-windows-64/wv2ray.exe -trimpath -ldflags "-s -w -H windowsgui -buildid=" ./main
 env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o v2ray-windows-64/v2ctl.exe -trimpath -ldflags "-s -w -buildid=" -tags confonly ./infra/control/main
 echo "使用 7z 打包中"
-7zz a ${cur_dir}/releases/v2ray-windows-64.7z v2ray.exe wv2ray.exe v2ctl.exe -mx=9 -ms -mf -mhc -mhcf -mmt -r
+7zz a ${cur_dir}/releases/v2ray-windows-64.7z v2ray.exe wv2ray.exe v2ctl.exe -mx=9 -r
 
 cd ${cur_dir}/v2ray-core-4.27.5/
 echo "移动 go.mod go.sum 至 releases/"
